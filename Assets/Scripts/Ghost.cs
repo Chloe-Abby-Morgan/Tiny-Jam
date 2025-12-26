b5d2f4f0d91e732f683e24bb8e60717d5b86e37d
Chloe Morgan <77995029+Chloe-Abby-Morgan@users.noreply.github.com>
2025-12-26 22:08:10 +0000

    Ghost Movement
    Darn prefabs

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -3,5 +3,25 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+    [SerializeField] private float speed = 1f;
+    private Transform player;
     public string colour = "green";
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
