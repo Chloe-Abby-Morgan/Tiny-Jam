f41df0a9c07bcf7d229aa1f4d9b0bd51d2030ba8
Chloe Morgan <77995029+Chloe-Abby-Morgan@users.noreply.github.com>
2025-12-27 02:49:32 +0000

    End of Day 1 Commit
    Lighting logic, I have been up for 21 hours; close but no cigar

---
+++ Assets/Scripts/LightingManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LightingManager : MonoBehaviour
+{
+
+    [SerializeField] private float lightingTime =5f;
+    [SerializeField] private SpriteRenderer[] ghostRenderers;
+    private GameObject[] ghosts;
+    private Color[] orginalColours;
+    private int index=0;
+    void Start()
+    {
+        Invoke("foudre", lightingTime);
+    }
+
+    void foudre()
+    {
+        ghosts = new GameObject[GameObject.FindGameObjectsWithTag("Ghost").Length];
+        ghostRenderers = new SpriteRenderer[GameObject.FindGameObjectsWithTag("Ghost").Length];
+        orginalColours = new Color[GameObject.FindGameObjectsWithTag("Ghost").Length];
+
+        index = 0;
+
+        ghosts = GameObject.FindGameObjectsWithTag("Ghost");
+
+        foreach(GameObject ghost in ghosts)
+        {
+            ghostRenderers[index] = ghost.GetComponent<SpriteRenderer>();
+            orginalColours[index] = ghostRenderers[index].color;
+            index++;
+        }
+
+        StartCoroutine(colourChange());
+
+        Invoke("foudre", lightingTime);
+    }
+
+    IEnumerator colourChange()
+    {
+        index = 0;
+
+        foreach(SpriteRenderer ghostRender in ghostRenderers)
+        {
+            if(ghostRender != null)
+            {
+                ghostRender.color = new Color(0, 0, 0, 1);
+            }
+        }
+        yield return new WaitForSeconds(lightingTime/2);
+
+        foreach(SpriteRenderer ghostRender in ghostRenderers)
+        {
+            if(ghostRender != null)
+            {
+                ghostRender.color = new Color(orginalColours[index].r, orginalColours[index].g, orginalColours[index].b, 1);
+            }
+            index++;
+            
+        }
+    }
+}
